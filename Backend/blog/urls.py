402bb39944d8decee9c4fcd84b0e159df460c1b5
Maxi <neumannmaximiliano@gmail.com>
2021-12-17 04:19:39 -0300

    arreglo error

--- Backend/blog/urls.py
+++ Backend/blog/urls.py
@@ -8,5 +8,5 @@ app_name='blog'
 
 urlpatterns = [
     path('', BlogHomePageView.as_view(), name='home'),
-    path('<slug>:<slug>/', PostDetailView.as_view(), name='post-detail'),
+    path('<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
 ]
